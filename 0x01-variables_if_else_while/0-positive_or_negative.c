d742b529ef6c71ee2fc3df4daac4b691bfd110d9
WaClare <cokumuh@gmail.com>
2023-02-17 12:54:52 -0800

    Variables

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,18 +2,13 @@
 int main(void)
 {
 	double num;
-	printf("n: ");
-
-	scanf("%lf", &num);
-	if (num <= 0.0)
-       	{
-		if (num == 0.0)
-			printf("You entered 0.");
-		else 
-			printf("You entered a negative number.");
+	printf("Enter a number: ");
+	scanf("%if", &num);
+	if (num > 0.00)
+		printf("is positive);
+	else if (num<0.00)
+		printf("is negative);
+	else
+		printf("is zero);
+	return 0;
 }
-else
-printf("You entered a positive number.");
-return 0;
-}
-
